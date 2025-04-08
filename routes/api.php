c997328db421e177ab40fc2e0e87e2fdb10a74a2
medlaq777 <m.laqsoumi@gmail.com>
2025-04-08 16:25:49 +0100

    create Profile routes

--- routes/api.php
+++ routes/api.php
@@ -8,8 +8,8 @@ Route::post('/login', [AuthController::class, 'login']);
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
-    Route::get('/profile', [ProfileController::class, 'show']);
-    Route::put('/profile', [ProfileController::class, 'update']);
-    Route::put('/profile/password', [ProfileController::class, 'updatePassword']);
-    Route::post('/profile/image', [ProfileController::class, 'updateProfileImage']);
+    // Route::get('/profile', [ProfileController::class, 'show']);
+    // Route::put('/profile', [ProfileController::class, 'update']);
+    // Route::put('/profile/password', [ProfileController::class, 'updatePassword']);
+    // Route::post('/profile/image', [ProfileController::class, 'updateProfileImage']);
 });
