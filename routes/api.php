a00d267cf9623cb7e0a936d212d26eb1373f2610
medlaq777 <m.laqsoumi@gmail.com>
2025-04-08 16:25:13 +0100

    fix all authentication error

--- routes/api.php
+++ routes/api.php
@@ -10,6 +10,6 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
     Route::get('/profile', [ProfileController::class, 'show']);
     Route::put('/profile', [ProfileController::class, 'update']);
-//     Route::put('/profile/password', [ProfileController::class, 'updatePassword']);
+    Route::put('/profile/password', [ProfileController::class, 'updatePassword']);
 //     Route::post('/profile/image', [ProfileController::class, 'updateProfileImage']);
 });
