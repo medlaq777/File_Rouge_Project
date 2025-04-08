e88e84aaa64f5798db69fed0dcfee59f00c99b8f
medlaq777 <m.laqsoumi@gmail.com>
2025-04-08 16:33:57 +0100

    handle the Profile model also make the relation with user

--- app/Models/Profile.php
+++ app/Models/Profile.php
@@ -9,4 +9,25 @@ class Profile extends Model
 {
     /** @use HasFactory<\Database\Factories\ProfileFactory> */
     use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'bio',
+        'profile_image',
+        'contact_info',
+        'address',
+        'city',
+        'country',
+        'phone',
+        'username',
+        'role',
+    ];
+    protected $hidden = [
+        'created_at',
+        'updated_at',
+    ];
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
