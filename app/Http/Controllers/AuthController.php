b0027abbd55833d9ee143042b72cc53bc44dc8f7
medlaq777 <m.laqsoumi@gmail.com>
2025-04-13 19:27:04 +0100

    Create Register Function in AuthController

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,8 +3,27 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
+use App\Services\AuthService;
 
 class AuthController extends Controller
 {
-    //
+    protected $authService;
+
+    public function __construct(AuthService $authService)
+    {
+        $this->authService = $authService;
+    }
+    public function register(Request $request)
+    {
+        $userData = $request->validate([
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:8|confirmed',
+            'role' => 'required|string|in:artist,owner',
+        ]);
+
+        $user = $this->authService->register($userData);
+
+        return response()->json(['user' => $user], 201);
+    }
 }
