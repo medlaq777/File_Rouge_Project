ec767ecd2f88d880153659309e0065c0341dc9c6
medlaq777 <m.laqsoumi@gmail.com>
2025-04-13 21:48:12 +0100

    make the ShowProfile Function in ProfileController

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -1,10 +1,14 @@
 <?php
 
 namespace App\Http\Controllers;
-
-use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Services\ProfileService;
 
 class ProfileController extends Controller
 {
-    //
+    public function showProfile()
+    {
+        $user = auth()->user();
+        return view('profile', compact('user'));
+    }
 }
---
+++ app/Services/ProfileService .php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\ProfileUser;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Auth;
+
+class ProfileService
+{
+
+}
