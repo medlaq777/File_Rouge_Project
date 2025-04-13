89cb044aa3bf5a1c9bc3d439ee2250126bba903d
medlaq777 <m.laqsoumi@gmail.com>
2025-04-13 14:43:03 +0100

    create profileUser Model, migration and factories

---
+++ app/Models/profileUser.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class profileUser extends Model
+{
+    /** @use HasFactory<\Database\Factories\ProfileUserFactory> */
+    use HasFactory;
+}
