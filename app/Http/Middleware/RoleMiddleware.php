80686ae22492eee0937967dd12363f6b34c8bcf3
medlaq777 <m.laqsoumi@gmail.com>
2025-04-13 14:30:53 +0100

    Create RoleMiddleware

---
+++ app/Http/Middleware/RoleMiddleware.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class RoleMiddleware
+{
+
+    public function handle($request, Closure $next, ...$roles)
+{
+    if (! in_array($request->user()->role->value, $roles)) {
+        abort(403, 'Accès refusé');
+    }
+
+    return $next($request);
+}
+}
