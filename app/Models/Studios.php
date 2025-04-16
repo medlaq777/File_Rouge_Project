28cb9b9db1acd2078f616f562d0fb32679dac8fd
medlaq777 <m.laqsoumi@gmail.com>
2025-04-16 11:24:32 +0100

    update the Studios Model and add studios seeders

--- app/Models/Studios.php
+++ app/Models/Studios.php
@@ -9,4 +9,25 @@ class Studios extends Model
 {
     /** @use HasFactory<\Database\Factories\StudiosFactory> */
     use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'description',
+        'address',
+        'location',
+        'price',
+        'availability',
+        'equipment',
+        'start_date',
+        'end_date',
+    ];
+    protected $casts = [
+        'start_date' => 'date',
+        'end_date' => 'date',
+        'availability' => 'boolean',
+    ];
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use App\Models\User;
 use App\Models\ProfileUser;
+use App\Models\Studios;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -16,6 +17,7 @@ class DatabaseSeeder extends Seeder
     {
         User::factory(10)->create()->each(function ($user) {
             ProfileUser::factory()->create(['user_id' => $user->id]);
+            Studios::factory(5)->create(['user_id' => $user->id]);
         });
     }
 }
