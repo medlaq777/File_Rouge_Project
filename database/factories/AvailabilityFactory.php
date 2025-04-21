07dbf4a7a93aa3fdbf14310fda076a9fafec37c4
medlaq777 <m.laqsoumi@gmail.com>
2025-04-21 16:24:26 +0100

    update the model availability and and studios for the relation , also make the migration for availability

--- database/factories/AvailabilityFactory.php
+++ database/factories/AvailabilityFactory.php
@@ -3,6 +3,8 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\User;
+use App\Models\Studios;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Availability>
@@ -17,7 +19,14 @@ class AvailabilityFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'user_id' => User::factory(),
+            'studio_id' => Studios::factory(),
+            'date' => $this->faker->date(),
+            'start_time' => $this->faker->time(),
+            'end_time' => $this->faker->time(),
+            'status' => $this->faker->randomElement(['available', 'booked']),
+            'created_at' => now(),
+            'updated_at' => now(),
         ];
     }
 }
