18af21093ca41a19e493a46e66c15aad23fe82ee
medlaq777 <m.laqsoumi@gmail.com>
2025-04-22 09:29:10 +0100

    Upgrade the studios Migration with make the studio name be unique

--- database/migrations/2025_04_16_100007_create_studios_table.php
+++ database/migrations/2025_04_16_100007_create_studios_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('studios', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->constrained()->onDelete('cascade')->onUpdate('cascade');
-            $table->string('name');
+            $table->string('name')->unique();
             $table->string('description')->nullable();
             $table->string('address')->nullable();
             $table->string('location')->nullable();
