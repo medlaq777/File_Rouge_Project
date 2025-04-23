cf55592655875e10021b0564084caae9d862c603
medlaq777 <m.laqsoumi@gmail.com>
2025-04-23 09:51:07 +0100

    update the photo_image migration

--- database/migrations/2025_04_20_141821_create_photos_table.php
+++ database/migrations/2025_04_20_141821_create_photos_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('studio_images', function (Blueprint $table) {
+        Schema::create('photos', function (Blueprint $table) {
             $table->id();
             $table->foreignId('studio_id')->constrained('studios')->onDelete('cascade');
             $table->string('image_path');
@@ -24,6 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('studio_images');
+        Schema::dropIfExists('photos');
     }
 };
