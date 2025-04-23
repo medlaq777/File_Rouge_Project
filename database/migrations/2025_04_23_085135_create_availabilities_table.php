de0c9f67d6b08b6c64c3619e21bf4f6d4c2f7734
medlaq777 <m.laqsoumi@gmail.com>
2025-04-23 09:53:09 +0100

    create the availabilities migration

---
+++ database/migrations/2025_04_23_085135_create_availabilities_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('availabilities', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('studio_id')->constrained('studios')->onDelete('cascade');
+            $table->date('date');
+            $table->time('start_time');
+            $table->time('end_time');
+            $table->boolean('is_booked')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('availabilities');
+    }
+};
