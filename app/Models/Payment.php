9626e9f91a75599c6b9c1455d4842fea3e5f4fff
medlaq777 <m.laqsoumi@gmail.com>
2025-04-23 10:28:36 +0100

    create model payment

---
+++ app/Models/Payment.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Payment extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'booking_id',
+        'user_id',
+        'amount',
+        'payment_date',
+        'transaction_id',
+        'method',
+        'status',
+    ];
+
+    protected $casts = [
+        'payment_date' => 'datetime',
+    ];
+
+    public function booking()
+    {
+        return $this->belongsTo(Booking::class, 'booking_id');
+    }
+
+    public function artist()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+
+    public function markAsSuccessful()
+    {
+        $this->statut = 'Success';
+        $this->save();
+    }
+
+    public function markAsFailed()
+    {
+        $this->statut = 'Failure';
+        $this->save();
+    }
+
+    public function refund()
+    {
+        $this->statut = 'refund';
+        $this->save();
+    }
+}
